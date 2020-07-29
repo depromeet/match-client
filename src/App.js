a638cf2cb8f80b481ede37acfe21f9da24447d72
andy nam <naamoonoo.y@gmail.com>
2020-07-29 20:42:11 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+import { SideBar } from "./SideBar";
 import "./App.css";
 
 function App() {
-	var mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
-
+	const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
 	mapboxgl.accessToken =
 		"pk.eyJ1IjoibmFhbW9vbm9vIiwiYSI6ImNrZDc3cDd3cTJud2wyeW15ajdnbnpxancifQ.Cfz9n6pSdx77lOZ0kU3nJQ";
 	var map = new mapboxgl.Map({
 		container: "map",
 		style: "mapbox://styles/mapbox/dark-v10",
-		center: [12.550343, 55.665957],
+		center: [127.024612, 37.5326],
 		zoom: 8,
 	});
 
 	var marker = new mapboxgl.Marker()
-		.setLngLat([12.550343, 55.665957])
+		.setLngLat([128.524612, 37.5326])
 		.addTo(map);
-
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<SideBar />
+			<SideBar width="300px" />
+			<input />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./App.css"
+
+export const SideBar = () => {
+
+
+	return <>
+		<div className="sidebar-left">
+
+		</div>
+	</>
+};
