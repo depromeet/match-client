7f49859de3fe895f1fd295132bd05aa87b7a8f8f
andy nam <naamoonoo.y@gmail.com>
2020-08-16 00:00:32 +0900

    [#4] feat: Input component work in progress

--- src/components/MainInput/MainInputComponent.jsx
+++ src/components/MainInput/MainInputComponent.jsx
@@ -1,9 +1,24 @@
-
-import React from 'react';
-import * as S from './styles';
+import React from "react";
+import * as S from "./styles";
+import { ReactComponent as Search } from "../../assets/images/search.svg";
+import { useMap } from "hooks/useMap";
 
 const MainInputComponent = () => {
-	return <S.Container>MainInputComponent</S.Container>;
+	const { viewport, onMove } = useMap();
+
+	return (
+		<S.Container>
+			<Search
+				onClick={() =>
+					onMove({
+						longitude: -74.1,
+						latitude: 40.7,
+					})
+				}
+			/>
+			<S.Input placeholder={"원하는 지역을 검색해보세요"} />
+		</S.Container>
+	);
 };
 
 export default MainInputComponent;
--- src/components/MainInput/styles.js
+++ src/components/MainInput/styles.js
@@ -2,18 +2,29 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 	position: fixed;
-	/* left: 0; */
-	right: 10%;
-	top: 10%;
-	width: 683px;
-	/* box-shadow: 12px 12px 2px 1px rgba(0, 0, 0, .2); */
+	width: 834px;
+	height: 52px;
+	left: 606px;
+	top: 32px;
+
 	background: #070b1e;
 	box-shadow: 0px 4px 4px rgba(7, 11, 30, 0.14);
-	border-radius: 8px;
 
 	border: none;
-	height: 30px;
 	color: white;
 	outline: none;
-	padding: 5px 10px;
+
+	display: flex;
+	align-items: center;
+`;
+
+export const Input = styled.input.attrs({
+	placeholderTextColor: "#EEEEEE",
+})`
+	outline: none;
+	border: none;
+	background: #070b1e;
+	width: 95%;
+	height: 100%;
+	color: white;
 `;
