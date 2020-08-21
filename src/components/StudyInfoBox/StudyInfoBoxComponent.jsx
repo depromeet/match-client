f95a46fb2fe8fbf27becf51b1e110f76b574dc84
kathy0917 <kathy95917@naver.com>
2020-08-22 00:31:27 +0900

    react-select 추가

--- src/components/StudyInfoBox/StudyInfoBoxComponent.jsx
+++ src/components/StudyInfoBox/StudyInfoBoxComponent.jsx
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./styles";
-//import { ReactComponent as CalendarIcon } from "img/ic_date.svg";
-import { CleanLink } from "components/CleanLink";
 import { useSelected } from "hooks/useSelected";
 import { useForm } from "react-hook-form";
 import { createGlobalStyle } from "../../styles/globalStyle";
 import { DatePicker } from "components/DatePicker";
+import Select from "react-select";
 
 const StudyInfoBoxComponent = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, setValue } = useForm();
   const onSubmit = (data) => console.log(data);
+  const options = [
+    { value: "00:00", label: "00:00" },
+    { value: "01:00", label: "01:00" },
+    { value: "02:00", label: "02:00" },
+    { value: "03:00", label: "03:00" },
+    { value: "04:00", label: "04:00" },
+    { value: "05:00", label: "05:00" },
+    { value: "06:00", label: "06:00" },
+    { value: "07:00", label: "07:00" },
+    { value: "08:00", label: "08:00" },
+    { value: "09:00", label: "09:00" },
+    { value: "10:00", label: "10:00" },
+    { value: "11:00", label: "11:00" },
+    { value: "12:00", label: "12:00" },
+    { value: "13:00", label: "13:00" },
+    { value: "14:00", label: "14:00" },
+    { value: "15:00", label: "15:00" },
+    { value: "16:00", label: "16:00" },
+    { value: "17:00", label: "17:00" },
+    { value: "18:00", label: "18:00" },
+    { value: "19:00", label: "19:00" },
+    { value: "20:00", label: "20:00" },
+    { value: "21:00", label: "21:00" },
+    { value: "22:00", label: "22:00" },
+    { value: "23:00", label: "23:00" },
+  ];
+
+  const handleMultiChange = (selectedOption) => {
+    setValue("reactSelect", selectedOption);
+    setReactSelect({ selectedOption });
+  };
+  const [values, setReactSelect] = useState({
+    selectedOption: [],
+  });
 
   return (
     <S.Container>
@@ -22,6 +55,16 @@ const StudyInfoBoxComponent = () => {
         <S.TitleInput name="stuff" ref={register} />
         <S.HR />
         <S.ContentInput />
+        <DatePicker />
+        <Select
+          className="reactSelect"
+          name="filters"
+          placeholder="Filters"
+          value={values.selectedOption}
+          options={options}
+          onChange={handleMultiChange}
+        />
+
         <S.CreateBtn type="submit">스터디 열기</S.CreateBtn>
         <S.SaveBtn>임시저장</S.SaveBtn>
       </S.Form>
@@ -41,8 +84,6 @@ const StudyInfoBoxComponent = () => {
         <S.MaxPeopleIcon />
         <S.TagIcon />
       </S.FlexIcon>
-
-      <DatePicker />
 
       <S.StartTime />
       <S.EndTime />
